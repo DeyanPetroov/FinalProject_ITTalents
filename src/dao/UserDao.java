441c42c0c1a5392a416814989b637cfcfcdcf807
NadyaPetrova <nadsss@abv.bg>
2018-04-09 20:47:51 +0300

    Added get user by id method

--- src/dao/UserDao.java
+++ src/dao/UserDao.java
@@ -12,6 +12,10 @@ import model.User;
 
 public class UserDao implements IUserDao {
 	
+	private static final String INSERT_USER_QUERY = "INSERT INTO users (first_name, last_name, email, age) VALUES (?,?,?,?)";
+	private static final String GET_USER_BY_ID = "SELECT * FROM users WHERE id = ?";
+	private static final String GET_ALL_USERS = "SELECT id, first_name, last_name, username, password, email, age FROM users";
+
 	private static UserDao instance;
 	private Connection connection;
 	
@@ -28,13 +32,23 @@ public class UserDao implements IUserDao {
 
 	@Override
 	public User getByID(int id) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		ResultSet resultSet = null;
+		User user = null;
+		try(PreparedStatement selectUserById = connection.prepareStatement(GET_USER_BY_ID);) {
+			selectUserById.setInt(1, id);
+			resultSet = selectUserById.executeQuery();
+			while (resultSet.next()) {
+				user = new User(resultSet.getString(1), resultSet.getString(2), resultSet.getString(3), resultSet.getString(4),resultSet.getString(5), resultSet.getInt(6));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return user;
 	}
 
 	@Override
 	public void saveUser(User u) throws SQLException {
-		PreparedStatement s = connection.prepareStatement("INSERT INTO users (first_name, last_name, email, age) VALUES (?,?,?,?)");
+		PreparedStatement s = connection.prepareStatement(INSERT_USER_QUERY);
 		s.setString(1, u.getFirstName());
 		s.setString(2, u.getLastName());
 		s.setString(3, u.getEmail());
@@ -50,7 +64,7 @@ public class UserDao implements IUserDao {
 
 	@Override
 	public Collection<User> getAllUsers() throws SQLException {
-		PreparedStatement s = connection.prepareStatement("SELECT id, first_name, last_name, username, password, email, age FROM users");
+		PreparedStatement s = connection.prepareStatement(GET_ALL_USERS);
 		HashSet<User> users = new HashSet<>();
 		ResultSet result = s.executeQuery();
 		while(result.next()) {
@@ -65,6 +79,4 @@ public class UserDao implements IUserDao {
 		}
 		return users;
 	}
-
 }
-
