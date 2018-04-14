9f06f408499be0d4232ae38aa069e4e22afdebc5
Deyan Petrov <didodiablo@abv.bg>
2018-04-14 13:37:13 +0300

    fix

--- src/model/dao/DBManager.java
+++ src/model/dao/DBManager.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class DBManager {
 	
-	private static final String DB_PASS = "Iloove3dg-"; //TODO: input on presentation
+	private static final String DB_PASS = ""; //TODO: input on presentation
 
 	private static final String DB_USER = "root";
 	private static final String DB_PORT = "3306";
